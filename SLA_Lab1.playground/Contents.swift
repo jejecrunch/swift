7381e9b2052a211320148eafa2f2f29a0399c258
유저 <user@yujeoui-MacBook-Pro.local>
2021-02-05 01:24:10 +0900

    210204 1장 연습 문제

---
+++ SLA_Lab1.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+/*
+ 개발자 이름 : 한지혜
+ 일자 : 2021/02/04
+ 프로젝트 이름 : SLA_Lab1
+ */
+
+// 이름 출력
+print("한지혜")
+
+// 주소 출력
+print("서울시")
+
+// 수식의 결과를 출력한다.
+print(27*82)
+print(541.33 - 61.888)
+print(999/540)
+
