a62c50f919990960ba90bfc901357ff0cf766608
유저 <user@yujeoui-MacBook-Pro.local>
2021-02-05 01:17:45 +0900

    210204 xcode project 실습

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by 유저 on 2021. 2. 5..
+//  Copyright © 2021년 LearnToProgram.tv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
