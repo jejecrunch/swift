ea9d4ddf9fd0947a0e927599d9645a0b7f0d2501
유저 <user@yujeoui-MacBook-Pro.local>
2021-02-05 00:44:38 +0900

    210204 swift 개요

---
+++ helloworld.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// var str = "Hello, playground"
+// var sum = 5 + 7
+
+print("Hello World")
+// addNumbers(1,2)
+
+print("Welcome to Swift")
+print(2 + 7)
